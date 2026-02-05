6d9e7269ae1a3fc2705047e2b62fca4396646354
Nofiniaina <nofyrnd@gmail.com>
2026-02-05 14:47:25 +0300

    add phpstan and rector

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+  ->withPaths([
+    __DIR__ . '/src',
+  ])
+  ->withSkip([
+    __DIR__ . '/vendor',
+
+  ])
+  // uncomment to reach your current PHP version
+  ->withPhpSets(php82: true)
+  ->withTypeCoverageLevel(0)
+  ->withDeadCodeLevel(0)
+  ->withCodeQualityLevel(0);
